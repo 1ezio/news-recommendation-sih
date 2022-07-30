3871dd706af2b918fd32e670ca3cc066a00003f8
Naman Mehra <naman.mehra,3000@gmail.com>
2022-07-31 00:59:19 +0530

    api updated

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from fastapi import FastAPI
 loaded_model = pickle.load(open("recommendation_model.sav", 'rb'))
 app = FastAPI()
 
-@app.get("/")
+@app.get("/recommendations")
 def getRecommendation(article):
     recommended = []
     for i , doc in enumerate(loaded_model.recommend(text = article,n = 5)):
         print(i+1)
-        recommended.append ( str(i+1)+"   "+" ".join(doc['text'].split()[:500]))
+        recommended.append ( str(i+1)+"  "+" ".join(doc['text'].split()[:500]))
         
     return [{
         "recommended":recommended
